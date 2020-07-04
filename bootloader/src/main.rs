2abcc4a672732b885bc4c7c62bc0484dbbce5493
ZiyanWu93 <wu000598@umn.edu>
2020-07-04 14:17:18 -0500

    panic handler can output error now

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,4 +1,4 @@
-#![feature(rustc_private)]
+#![feature(rustc_private,panic_info_message)]
 #![no_std]
 #![no_main]
 
@@ -7,17 +7,36 @@ mod core_reqs;
 use core::panic::PanicInfo;
 
 use serial::print;
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
 
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    print!("PANIC:");
+
+    if let Some(loc) = info.location() {
+        print!(" {}:{}:{}", loc.file(), loc.line(),
+               loc.column());
+    }
+
+    if let Some(msg) = info.message() {
+        print!(" {}", msg);
+    }
+
+    print!("\n");
+
+    cpu::halt();
+}
 
 
 #[no_mangle]
 extern fn entry() {
     serial::init();
-    print!("Welcome to the chocolate milk! {:?}\n", 2);
+    let mem= [0u8;10];
+    let mem= [0u8;10];
+    print!("{:?}\n",mem[0]);
+    print!("{:?}\n",mem[..][0]);
+    print!("{:?}\n",mem[..][50]);
+    // let val=5;
+    // print!("Welcome to the chocolate milk! {:p}\n", &val);
     cpu::halt();
 }
 
